f3ec876ef0c0b121203a5375c496a393b243c264
César Alejandro Arteaga Gonzalez <alexglez41199@gmail.com>
2023-08-10 16:17:29 -0600

    d

--- static-web/astro.config.mjs
+++ static-web/astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://Cesar41199.github.io",
+  site: "https://rithware.com.mx",
+  base: "/Cesar41199",
 });
